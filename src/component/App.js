86751ae16e46e528bc734ab8272d159b7f7d91da
pankaj <pankajkumarsingh761@gmail.com>
2018-05-12 23:44:09 +0530

    passing data with props and loop on array

--- src/component/App.js
+++ src/component/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
 import { Header } from "../component/Header"
 import { Home } from "../component/Home"
 
 class App extends Component {
   render() {
+      var user={
+          name:"Pankaj",
+          age:27,
+          hobies:["cricket","music"]
+      };
     return (
       <div className="container">
         <Header/>
-        <Home/>
+        <Home user={user} />
       </div>
     );
   }
--- src/component/Home.js
+++ src/component/Home.js
@@ -2,20 +2,21 @@ import React from "react";
 
 export class Home extends React.Component{
     render(){
-        let constabt;
-        if(true){
-            constabt=<p>Hello Sir !!<br/>HOw are you ?</p>
-        }
+        console.log(this.props)
         return (
             <div>
                 <p>
-                    thisjhkgsjkg  <br/>
-                    erwfewfweefgew yuefgyuwe uygwe weyugf wegfyuwe g<br/>
-                    jhyfgewyufguy yufwey yuewfd uyfyuwq uy fwquyyu <br/>
+                 Hello World !!<br/>
+                    <p>
+                        your name is : {this.props.user.name} and age is : {this.props.user.age}
+                    </p>
+                    <div>
+                        <h4>Hobbies</h4>
+                        <ul>
+                            {this.props.user.hobies.map(x=><li>{x}</li>)}
+                        </ul>
+                    </div>
                 </p>
-                {2+2}
-                <br/>
-                {constabt}
             </div>
         );
     }
